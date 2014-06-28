a09dae83dee4ab65b528be97c86f59b0b06139c2
Dmitry Sobornykov <d1984v@gmail.com>
2014-06-28 15:20:32 +0300

    order objects and fields

--- src/OpenCartAccess/Models/Order/OpenCartOrder.cs
+++ src/OpenCartAccess/Models/Order/OpenCartOrder.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace OpenCartAccess.Models.Order
 {
@@ -8,7 +10,71 @@ namespace OpenCartAccess.Models.Order
 		[ DataMember( Name = "order_id" ) ]
 		public int OrderId { get; set; }
 
+		[ DataMember( Name = "date_added" ) ]
+		public DateTime CreatedDate { get; set; }
+
 		[ DataMember( Name = "order_status_id" ) ]
 		public string StatusId { get; set; }
+
+		[ DataMember( Name = "total" ) ]
+		public decimal OrderTotal { get; set; }
+
+		[ DataMember( Name = "email" ) ]
+		public string Email { get; set; }
+
+		[ DataMember( Name = "telephone" ) ]
+		public string Phone { get; set; }
+
+		[ DataMember( Name = "payment_firstname" ) ]
+		public string PaymentFirstName { get; set; }
+
+		[ DataMember( Name = "payment_lastname" ) ]
+		public string PaymentLastName { get; set; }
+
+		[ DataMember( Name = "payment_company" ) ]
+		public string PaymentCompany { get; set; }
+
+		[ DataMember( Name = "shipping_address_1" ) ]
+		public string ShippingAddress1 { get; set; }
+
+		[ DataMember( Name = "shipping_address_2" ) ]
+		public string ShippingAddress2 { get; set; }
+
+		[ DataMember( Name = "shipping_postcode" ) ]
+		public string ShippingPostCode { get; set; }
+
+		[ DataMember( Name = "shipping_city" ) ]
+		public string ShippingCity { get; set; }
+
+		[ DataMember( Name = "shipping_country" ) ]
+		public string ShippingCountry { get; set; }
+
+		[ DataMember( Name = "shipping_zone" ) ]
+		public string ShippingZone { get; set; }
+
+		[ DataMember( Name = "shipping_method" ) ]
+		public string ShippingMethod { get; set; }
+
+		[ DataMember( Name = "products" ) ]
+		public IList< OpenCartOrderProduct > Products { get; set; }
+	}
+
+	public enum OpenCartOrderStatusEnum
+	{
+		MissingOrder,
+		Canceled,
+		CanceledReversal,
+		Chargeback,
+		Complete,
+		Denied,
+		Expired,
+		Failed,
+		Pending,
+		Processed,
+		Processing,
+		Refunded,
+		Reversed,
+		Shipped,
+		Voided
 	}
 }
---
+++ src/OpenCartAccess/Models/Order/OpenCartOrderProduct.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace OpenCartAccess.Models.Order
+{
+	[ DataContract ]
+	public class OpenCartOrderProduct
+	{
+		[ DataMember( Name = "sku" ) ]
+		public string Sku { get; set; }
+
+		[ DataMember( Name = "quantity" ) ]
+		public int Quantity { get; set; }
+
+		[ DataMember( Name = "price" ) ]
+		public decimal Price { get; set; }
+	}
+}
